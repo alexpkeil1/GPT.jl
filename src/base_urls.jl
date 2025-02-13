d08dc9b3b198d2b9a2eedfb259d977010292cd90
Alex Keil <alexpkeil1@users.noreply.github.com>
2025-02-13 12:20:52 -0500

    bug

--- src/base_urls.jl
+++ src/base_urls.jl
@@ -3,13 +3,14 @@
 
 const url = NamedTuple([ 
   :chats=>"https://api.openai.com/v1/chat/completions", 
-  :completions=>"https://api.openai.com/v1/completions", 
+  :completions=>"https://api.openai.com/v1/chat/completions", # original was deprecated
   :embeddings=>"https://api.openai.com/v1/embeddings", 
   :generations=>"https://api.openai.com/v1/images/generations", 
   :edits=>"https://api.openai.com/v1/images/edits",
   :variations=>"https://api.openai.com/v1/images/variations", 
   :fine_tune=>"https://api.openai.com/v1/fine-tunes",
   :assistants=>"https://api.openai.com/v1/assistants",
-  :file_upload=>"https://api.openai.com/v1/files"
+  :file_upload=>"https://api.openai.com/v1/files",
+  :fine_tuning=>"https://api.openai.com/v1/fine_tuning/jobs"
   ])
   
